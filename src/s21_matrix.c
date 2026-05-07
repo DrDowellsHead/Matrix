79fc03406851b78ba249d14798b5a121b1804f59
Ivan Perfilov <lordvan2013@gmail.com>
2026-05-07 15:59:18 +0300

    Added s21_sum_matrix

--- src/s21_matrix.c
+++ src/s21_matrix.c
@@ -57,20 +57,50 @@ int s21_eq_matrix(s21_matrix *A, s21_matrix *B) {
 		return 1;
 		}
 		
-		for (int r1 = 0; r1< A->rows; r1++) {
-			for (int r2 = 0; r2 < B-> rows; r2++) {
-				if (A->matrix[i] != B->matrix[i]) {
-					return 2;
-					}
+	for (int r1 = 0; r1< A->rows; r1++) {
+		for (int r2 = 0; r2 < B-> rows; r2++) {
+			if (A->matrix[i] != B->matrix[i]) {
+				return 2;
 				}
+			}
 			
-		for (int c1 = 0; c1< A->columns; c1++) {
-			for (int c2 = 0; c2 < B-> columns; c2++) {
-				if (A->matrix[i] != B->matrix[i]) {
-					return 2;
-					}
+	for (int c1 = 0; c1< A->columns; c1++) {
+		for (int c2 = 0; c2 < B-> columns; c2++) {
+			if (A->matrix[i] != B->matrix[i]) {
+				return 2;
 				}
+			}
 			
-		return 0;
-		}
+	return 0;
+}
+	
+int s21_sum_matrix(s21_matrix *A, s21_matrix *B, s21_matrix *result) {
+	sum_row = [];
+	sum_col = [];
+	
+	if (A->matrix == NULL || B->matrix == NULL) {
+		return 1;
 	}
+		
+	for (int r1 = 0; r1 < A->rows; r1++) {
+		for (int r2 = 0; r2 < B->rows; r2++)
+			sum_row = A->matrix[i] + B->matrix[i];
+			
+			if (r1 == NULL || r2 == NULL) {
+				return 2;
+			}
+		return 0;
+	}
+	
+	for (int c1 = 0; c1 < A->rows; c1++) {
+		for (int c2 = 0; c2<B->columns; c2++) {
+			sum_row = A->matrix[i] + B->matrix[i];
+			
+			if (c1 == NULL || c2 == NULL) {
+				return 2;
+			}
+		}
+		
+		return 0;
+	}
+}
