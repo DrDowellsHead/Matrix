a93ad8d83e3d14879c24ae4a2f8120bb88bb5d94
Ivan Perfilov <lordvan2013@gmail.com>
2026-05-11 00:05:19 +0300

    Fixed mistakes

--- src/s21_mult_matrix.c
+++ src/s21_mult_matrix.c
@@ -1,6 +1,6 @@
-#include "s21_string.h"
+#include "s21_matrix.h"
 
-int s21_mult_number(s21_matrix *A, double number, s21_matrix *result) {
+int s21_mult_matrix(s21_matrix *A, s21_matrix *B, s21_matrix *result) {
 	if (s21_validate_matrix(A) != 0 || s21_validate_matrix(B) != 0 || result == NULL) {
 		return 1;
 	}
