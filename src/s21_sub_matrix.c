c65b7959be0c91bdb2c20cee1c648792fddd82b5
Ivan Perfilov <lordvan2013@gmail.com>
2026-05-10 19:32:41 +0300

    Added s21_sub_matrix.c.

--- src/s21_sub_matrix.c
+++ src/s21_sub_matrix.c
@@ -1,17 +1,23 @@
 #include "s21_matrix.h"
 
 int s21_sub_matrix(s21_matrix *A, s21_matrix *B, s21_matrix *result) {
-    if (A == NULL || B == NULL) {
-        return 1;
+    if (s21_validate_matrix != 0) {
+		return 1;
+	}
+
+    if (!s21_dimensions_equal(A, B)) {
+        return 2;
     }
-
-    if (s21_eq_matrix) {
-        for (int r1 = 0; r1 < A->rows; r1++) {
-            for (int r2 = 0; r2 < B->rows; r2++) {
-
-            }
+	
+	if (s21_create_matrix(A->rows, A->columns, result) != 0) {
+		return 1;
+	}
+	
+    for (int i = 0; i < A->rows; i++) {
+        for (int j = 0; j < A->columns; j++) {
+			result->matrix[i][j] = A->matrix[i][j] - B->matrix[i][j];
         }
     }
 
-    return 2;
+    return 0;
 }
