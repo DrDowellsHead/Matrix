b6e5801d50268b8a47528ade382b6da14f53a313
Ivan Perfilov <lordvan2013@gmail.com>
2026-06-07 14:33:42 +0300

    Realized s21_calc_complements.c. Added helper function s21_minor_matrix for transformation original matrix to minor matrix.

--- src/s21_calc_complements.c
+++ src/s21_calc_complements.c
@@ -5,9 +5,44 @@ int s21_calc_complements(s21_matrix *A, s21_matrix *result) {
         return 1;
     }
 
-    if (s21_create_matrix(A->rows - 1, A->columns - 1, result) != 0) {
+    if (A->rows != A->columns) {
+        return 2;
+    }
+
+    if (s21_create_matrix(A->rows, A->columns, result) != 0) {
         return 1;
     }
 
-    
+    if (A->rows == 1) {
+        result->matrix[0][0] = 1;
+        return 0;
+    }
+
+    for (int i = 0; i < A->rows; i++) {
+        for (int j = 0; j < A->columns; j++) {
+            s21_matrix minor;
+            double det = 0;
+
+            if (s21_minor_matrix(A, i, j, &minor) != 0) {
+                s21_remove_matrix(result);
+                return 1;
+            }
+
+            if (s21_determinant(&minor, &det) != 0) {
+                s21_remove_matrix(&minor);
+                s21_remove_matrix(result);
+                return 1;
+            }
+
+            if ((i + j) % 2 == 0) {
+                result->matrix[i][j] = det;
+            } else {
+                result->matrix[i][j] = -det;
+            }
+
+            s21_remove_matrix(&minor);
+        }
+    }
+
+    return 0;
 }
---
+++ src/s21_minor_matrix.c
@@ -0,0 +1,39 @@
+#include "s21_matrix.h"
+
+int s21_minor_matrix(s21_matrix *A, int excluded_row, int excluded_col,
+                     s21_matrix *result) {
+    if (s21_validate_matrix(A) != 0 || result == NULL) {
+        return 1;
+    }
+
+    if (s21_create_matrix(A->rows - 1, A->columns - 1, result) != 0) {
+        return 1;
+    }
+
+    if (A->rows <= 1 || A->columns <= 1) {
+        return 2;
+    }
+
+    int minor_i = 0;
+
+    for (int i = 0; i < A->rows; i++) {
+        if (i == excluded_row) {
+            continue;
+        }
+
+        int minor_j = 0;
+
+        for (int j = 0; j < A->columns; j++) {
+            if (j == excluded_col) {
+                continue;
+            }
+
+            result->matrix[minor_i][minor_j] = A->matrix[i][j];
+            minor_j++;
+        }
+
+        minor_i++;
+    }
+
+    return 0;
+}
