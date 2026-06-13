f1648c33d439df051ba94e90df5f101833e082a5
Ivan Perfilov <lordvan2013@gmail.com>
2026-06-13 12:42:10 +0300

    Added s21_determinant.c

---
+++ src/s21_determinant.c
@@ -0,0 +1,50 @@
+#include "s21_matrix.h"
+
+int s21_determinant(s21_matrix *A, double *result) {
+    if (s21_validate_matrix(A) != 0 || result == NULL) {
+        return 1;
+    }
+
+    if (A->rows != A->columns) {
+        return 2;
+    }
+
+    if (A->rows == 1) {
+        *result = A->matrix[0][0];
+        return 0;
+    }
+
+    if (A->rows == 2) {
+        *result = A->matrix[0][0] * A->matrix[1][1] -
+                  A->matrix[0][1] * A->matrix[1][0];
+        return 0;
+    }
+
+    double det = 0;
+
+    for (int j = 0; j < A->columns; j++) {
+        s21_matrix minor;
+        double minor_det = 0;
+
+        if (s21_minor_matrix(A, 0, j, &minor) != 0) {
+            return 1;
+        }
+
+        if (s21_determinant(&minor, &minor_det) != 0) {
+            s21_remove_matrix(&minor);
+            return 1;
+        }
+
+        if (j % 2 == 0) {
+            det += A->matrix[0][j] * minor_det;
+
+        } else {
+            det -= A->matrix[0][j] * minor_det;
+        }
+
+        s21_remove_matrix(&minor);
+    }
+
+    *result = det;
+    return 0;
+}
